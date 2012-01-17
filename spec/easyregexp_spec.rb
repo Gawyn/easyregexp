0c723dc2d86093d8a2be845771a2901d7b464032
Gawyn <cplanas.work@gmail.com>
2012-01-17 13:25:27 +0100

    Specs testing attributes and first method

--- spec/easyregexp_spec.rb
+++ spec/easyregexp_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
-describe Easyregexp do
-  pending "write it"
+describe Easyregexp::Easyregexp do
+  describe 'attributes' do
+    #remember to use attr_accesor !!!!
+    it { should_respond_to :regexp }
+    it { should_respond_to :verbose }
+  end
+  describe 'generating methods' do
+    describe '#anything_but_whitespaces' do
+      it 'returns the correct regexp' do
+        subject.anything_but_whitespaces
+        subject.regexp.should = /\S*/
+      end
+      it 'should behave properly accepting' do
+        subject.anything_but_whitespaces
+        'AAAAA'.should =~ subject
+      end
+      it 'should behave properly rejecting' do
+        subject.anything_but_whitespaces
+        'A AAAA'.should =~ subject
+      end
+    end
+  end
 end
