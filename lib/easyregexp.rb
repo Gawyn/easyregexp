bd7796ef8df91966afd74d3a402a9cfa1fc623bc
Gawyn <cplanas.work@gmail.com>
2012-02-03 23:46:35 +0200

    Structure of the project modified

--- lib/easyregexp.rb
+++ lib/easyregexp.rb
@@ -1,3 +1,29 @@
 module Easyregexp
-  # Your code goes here...
+
+  class Easyregexp
+
+    attr_reader :verbose, :regexp
+
+    def anything_but_whitespaces
+      if @regexp.nil?
+        @regexp = Regexp.new('\S*')
+      end
+      verbalize(__method__)
+    end
+
+    def anything_but(arg)
+      if @regexp.nil?
+        @regexp = Regexp.new('\/'+arg.to_s+'*')
+      end
+      verbalize(__method__)
+    end
+
+    private
+
+    def verbalize(method)
+      @verbose = method.to_s.gsub!('_',' ').capitalize
+    end
+
+  end
+
 end
--- spec/easyregexp_spec.rb
+++ spec/easyregexp_spec.rb
@@ -1,25 +1,36 @@
 require 'spec_helper'
 describe Easyregexp::Easyregexp do
   describe 'attributes' do
-    #remember to use attr_accesor !!!!
   end
   describe 'generating methods' do
-    describe '#anything_but_whitespaces' do
-      it 'returns the correct regexp' do
-        subject.anything_but_whitespaces
-        subject.regexp.should = /\S*/
+    describe '#anything_but_?' do
+      describe '#anything_but()' do
+        it 'returns the correct regexp' do
+          subject.anything_but('a')
+          subject.regexp.should == /[^a]/
+        end
+        it 'returns the correct verbose form' do
+          subject.anything_but('b')
+          subject.verbose.should == 'Anything but b'
+        end
+        it 'behaves properly' do
+          subject.anything_but('c')
+          subject.regexp.should =~ 'cccc'
+        end
       end
-      it 'returns the correct verbose form' do
-        subject.anything_but_whitespaces
-        subject.verbose.should = 'Anything but whitespaces'
-      end
-      it 'should behave properly accepting' do
-        subject.anything_but_whitespaces
-        'AAAAA'.should =~ subject
-      end
-      it 'should behave properly rejecting' do
-        subject.anything_but_whitespaces
-        'A AAAA'.should =~ subject
+      describe '#anything_but_whitespaces' do
+        it 'returns the correct regexp' do
+          subject.anything_but_whitespaces
+          subject.regexp.should == /\S*/
+        end
+        it 'returns the correct verbose form' do
+          subject.anything_but_whitespaces
+          subject.verbose.should == 'Anything but whitespaces'
+        end
+        it 'should behave properly accepting' do
+          subject.anything_but_whitespaces
+          'AAAAA'.should =~ subject.regexp
+        end
       end
     end
   end
