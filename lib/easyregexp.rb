8e6c9cf7796802e9bc70666e679785e125aa449e
Gawyn <cplanas.work@gmail.com>
2012-02-04 00:13:58 +0200

    Anything_but methods working

--- lib/easyregexp.rb
+++ lib/easyregexp.rb
@@ -5,23 +5,22 @@ module Easyregexp
     attr_reader :verbose, :regexp
 
     def anything_but_whitespaces
-      if @regexp.nil?
-        @regexp = Regexp.new('\S*')
-      end
+      @regexp ||= Regexp.new('\S*')
       verbalize(__method__)
     end
 
     def anything_but(arg)
-      if @regexp.nil?
-        @regexp = Regexp.new('\/'+arg.to_s+'*')
-      end
-      verbalize(__method__)
+      @regexp ||= Regexp.new('[^'+arg.to_s+']')
+      verbalize(__method__, arg)
     end
 
     private
 
-    def verbalize(method)
-      @verbose = method.to_s.gsub!('_',' ').capitalize
+    def verbalize(method, arg = '')
+      unless arg.empty?
+        arg = ' '+arg
+      end
+      @verbose = method.to_s.gsub!('_',' ').capitalize+arg
     end
 
   end
