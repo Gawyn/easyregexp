45159e97ab58df1297cc0a81d0dc2bd4ecd7c062
Gawyn <cplanas.work@gmail.com>
2012-02-07 03:11:03 +0200

    All_the_whitespaces method working

--- lib/easyregexp.rb
+++ lib/easyregexp.rb
@@ -4,6 +4,8 @@ module Easyregexp
 
     attr_reader :verbose, :regexp
 
+    #Change the way the gem works; have a hash with the name of the methods and the regular expression simbols
+
     def anything_but_whitespaces
       @regexp ||= Regexp.new('\S*')
       verbalize(__method__)
@@ -14,6 +16,11 @@ module Easyregexp
       arg = arg.inject(:+) if arg.is_a? Array
       @regexp ||= Regexp.new('[^'+arg.to_s+']')
       verbalize(__method__, arg)
+    end
+
+    def all_the_whitespaces
+      @regexp ||= Regexp.new('\s')
+      verbalize(__method__)
     end
 
     private
